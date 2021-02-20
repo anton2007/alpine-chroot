61b79079d0b85edcfdb474c7150c86da5ef671fb
anton2007 <37032392+anton2007@users.noreply.github.com>
2021-02-20 07:49:06 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,29 @@
+DIR="alpine-rootfs/"
+VER="3.13"
+SUBVER="0"
+ARCH="armv7"
+
+SetupChroot() {
+	mkdir "$DIR"
+	tar -zxf alpine-minirootfs-"$VER"."$SUBVER"-"$ARCH".tar.gz -C "$DIR"
+	echo "cd $DIR" > start.sh
+	echo "busybox mount --bind /dev dev" >> start.sh
+	echo "mount -t devpts devpts dev/pts" >> start.sh
+	echo "mount -t proc proc proc" >> start.sh
+	echo "mount -t sysfs sysfs sys" >> start.sh
+	echo "export PATH="$PATH:/usr/sbin:/sbin:/bin"" >> start.sh
+	echo "chroot . /bin/sh" >> start.sh
+	echo "umount dev/pts" >> start.sh
+	echo "umount dev" >> start.sh
+	echo "umount proc" >> start.sh
+	echo "umount sys" >> start.sh
+	chmod 777 start.sh
+}
+
+if [ -e alpine-minirootfs-"$VER"."$SUBVER"-"$ARCH".tar.gz ]
+then
+	SetupChroot	
+else
+	wget https://dl-cdn.alpinelinux.org/alpine/v"$VER"/releases/"$ARCH"/alpine-minirootfs-"$VER"."$SUBVER"-"$ARCH".tar.gz
+	SetupChroot
+fi
